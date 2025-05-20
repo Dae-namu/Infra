505afc373b25a14378a420b89aa304e9707df708
chanhk1 <changal1234@naver.com>
2025-05-20 16:00:22 +0900

    sg 생성, main 수정

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
 module "eks" {
   source = "./modules/eks"
 
-  subnet_ids                 = module.vpc.subnet_ids
+  subnet_ids                 = module.vpc.public_subnet_ids
   cluster_security_group_ids = ["aws_security_group.eks-nodes-sg"]
   cluster_name               = "my-eks-cluster"
   cluster_role_arn           = aws_iam_role.eks_cluster_role.arn
--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -13,3 +13,7 @@ output "cluster_endpoint" {
   value       = aws_eks_cluster.eks_cluster.endpoint
 }
 
+output "cluster_role_arn" {
+  description = "EKS Role ARN"
+  value       = aws_iam_role.eks_cluster_role.arn
+}
